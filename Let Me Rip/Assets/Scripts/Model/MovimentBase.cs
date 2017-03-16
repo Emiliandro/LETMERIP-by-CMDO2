b3cdd70d21d60fbd477e2b3cc00bdcfe0f4fc557
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2017-03-16 15:00:17 -0400

    Player Moviment

---
+++ Let Me Rip/Assets/Scripts/Model/MovimentBase.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentBase : MonoBehaviour {
+
+
+    private Animator anim;
+    private Transform tf;
+    private Rigidbody2D rb2D;
+
+    [SerializeField]
+    private Transform groundedCheck;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private bool isGrounded;
+
+    [SerializeField]
+    private bool facingRight;
+
+    public float jumpForce = 10.0f;
+    public float speed;
+    public float maxSpeed;
+
+    public bool canDash = true;
+    public float dashCooldown = 2f;
+    public float dashSpeed = 3;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        rb2D = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = Physics2D.OverlapCircle(groundedCheck.position, 0.2f, groundLayer);
+
+        float h = Input.GetAxis("Horizontal");
+
+        Move(h);
+        if (Input.GetKeyDown(KeyCode.Z)){
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && canDash){
+            StartCoroutine(DashBoost(dashCooldown));
+        }
+
+
+	}
+
+    public void Move(float direction){
+
+        rb2D.AddForce((Vector2.right * speed) * direction);
+
+        if (rb2D.velocity.x > maxSpeed){
+            rb2D.velocity = new Vector2(maxSpeed, rb2D.velocity.y);
+        }
+        if(rb2D.velocity.x < -maxSpeed){
+            rb2D.velocity = new Vector2(-maxSpeed, rb2D.velocity.y);
+        }
+
+        if (direction > 0 && !facingRight)
+            Flip();
+        if (direction < 0 && facingRight)
+            Flip();
+
+
+    }
+
+    private void Flip(){
+        facingRight = !facingRight;
+        
+    }
+
+    public void Jump(){
+        if (isGrounded){
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    public IEnumerator DashBoost(float dashDur){
+
+        canDash = false;
+
+        if (facingRight){
+            rb2D.AddForce(Vector2.right * dashSpeed);
+        }else{
+            rb2D.AddForce(Vector2.right * -dashSpeed);
+        }
+
+        yield return new WaitForSeconds(dashDur);
+        canDash = true;
+
+    }
+
+
+}
---
+++ Let Me Rip/Assets/Scripts/Model/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour{
+
+
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+
+    float accelarationTimeAirbone = .2f;
+    float accelarationTimeGrounded = .1f;
+
+    private float gravity = -20;
+    private Vector3 velocity;
+
+    float velocityXSmoothing;
+
+    private float jumpVelocity;
+    public float moveSpeed;
+
+
+    Controller2D controller;
+
+    void Start(){
+        controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        Debug.Log("Gravity: " + gravity);
+        Debug.Log("JumpVelocity: " + jumpVelocity);
+
+    }
+
+    private void Update(){
+
+        if(controller.collisions.above || controller.collisions.bellow)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Z) && controller.collisions.bellow){
+            velocity.y = jumpVelocity;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.bellow)?accelarationTimeGrounded:accelarationTimeAirbone);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+}
